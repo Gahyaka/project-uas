3ecc234daee6dbfa0bc23e9587617ef394fcbb6b
Gahyaka <putragahyaka@gmail.com>
2024-09-10 13:53:52 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-def add_book(title, author):
-    book = {
-        'title': title,
-        'author': author
-    }
-    book_list.append(book)
-    print(f"Buku '{title}' oleh {author} telah ditambahkan!")
+print ("Hello World")
+print ("Assalamualaikum"}
